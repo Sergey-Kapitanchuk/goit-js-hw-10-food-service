22d3869699723821f8bcb328935ccaa064c34c42
Sergey-Kapitanchuk <kapitanchukserg28@gmail.com>
2021-10-19 01:39:27 +0300

    finsh

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
-import './menu.json';
+import menuCards from './templates/menu-cards.hbs'
+import menuList from './menu.json';
+
+const cardItemMenu = menuCards({ menuList });
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+};
 
 const refs = {
-
+    menu: document.querySelector('.js-menu'),
+    bodyListener: document.querySelector('body'),
+    checkBox: document.getElementById('theme-switch-toggle'),
 }
+
+refs.menu.insertAdjacentHTML('beforeend', cardItemMenu);
+
+refs.checkBox.addEventListener('change', themeEvent);
+
+checkedEvtListener()
+
+function themeEvent() {
+    const check = refs.checkBox.checked;
+    if (check === true) {
+        refs.bodyListener.classList.remove(Theme.LIGHT);
+        refs.bodyListener.classList.add(Theme.DARK);
+    }
+    if (check !== true) {
+        refs.bodyListener.classList.remove(Theme.DARK);
+        refs.bodyListener.classList.add(Theme.LIGHT);
+    }
+   localStorage.setItem('theme', check)
+   
+};
+
+function checkedEvtListener() {
+    const savedCheck = JSON.parse(localStorage.getItem('theme'));
+    if(savedCheck) {
+        refs.checkBox.checked = savedCheck;
+        themeEvent()
+        }
+}
+
+
